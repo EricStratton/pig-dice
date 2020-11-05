bd846703acc5629c03ef5b18fd0f10fdb038472a
Eric Stratton <strattonericj@gmail.com>
2020-11-04 16:29:30 -0800

    condense player selection function and adding roll to turn total into one function
    Co-authored-by: Nathan Schrader <nathanschrader@icloud.com

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 | "It should generate a random number between 1 & 6 on Submit" | Click "Roll" button | Random 1-6 |
 | randomNumberEvaluator(); |||
 | "It will see if output of randomNumber() === 1" | randomNumber() | true/false |
+| "If randomNumber() ! === 1, add number to turnTotal" | randomNumber() | turnTotal += randomNumber |
+| "If randomNumber() === 1, turnTotal = 0 and other player gets dice" | randomNumber() | 
--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,57 @@
 // Business Logic //
+let player1;
+let player2;
+
+function Player (name, turnTotal, overAll, active) {
+  this.name = name
+  this.turnTotal = 0;
+  this.overAll = 0;
+  this.active = active;
+}
+
+Player.prototype.addScore = function (turnTotal, overAll) {
+  this.overAll += this.turnTotal;
+  if (this.overAll >= 100) {
+    alert("Winner!");
+  } else {
+    return this.overAll
+  }
+}
 
 function randomNum() {
   let roll = Math.floor(Math.random() * 7);
   return roll;
 }
 
-
-function randomNumEvaluator(roll) {
-  if (roll === 1) {
-    return false
-  }
-  else {
-    player1Total += roll
-  }
+function playerGoesFirst(roll); {
+  if (roll === 0 - .5)  {
+    return True // ("#player1Area").show();
+  } else {
+    ("#player2Area").show();
+  } 
 }
 
-
-
-
-
+function rollEvaluator (roll) {
+  if (roll === 1) {
+    if (this.activate === player1.active) {
+      player1.active = false;
+      player2.active = true;
+    } else {
+      player2.active = false;
+      player1.active = true;
+    }
+  if (playerTotal >= 100) {
+    player wins game;
+  }
+  } else {
+    this.turnTotal += roll;
+  }  
+}
 
 
 
 // User Interface //
 
-let roll;
-let player1total = 0;
+
 
 randomNumEvaluator()
