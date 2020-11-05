f0d114851b755409530596e8f5e62d35cd365a68
Eric Stratton <strattonericj@gmail.com>
2020-11-05 13:34:59 -0800

    add beginning of refactoring code to make functional and less busy
    Co-authored-by: Nathan Schrader <nathanschrader@icloud.com

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,81 @@
 // Business Logic //
-let player1;
-let player2;
+
+function Player() {
+  this.turnScore = 0;
+  this.globalScore = 0;
+};
+
+// Player Objects
+let player1 = new Player();
+let player2 = new Player();
+
+// playerx function to roll dice
+Player.prototype.rollDice = function()  {
+  this.dice = Math.floor(Math.random() * 6) + 1; // generates random num 1-6
+  if (this.dice === 1) {  // if roll is "1"
+    return false; // return to playerX.rollDice result === false in UI
+  } else (this.dice !== 1) {
+    this.turnScore += this.dice;
+    return this.turnScore;  // return to playerX.rollDice 'else' in UI
+  }
+};
+
+// User Interface Logic //
+
+$(document).ready(function() {
+  $("#player1").text(player1.turnScore = 0);
+  $("#player2").text(player2.turnScore = 0);
+  $("#player1").text(player1.globalScore = 0);
+  $("#player2").text(player2.globalScore = 0);
+
+//********************************************************************************************//
+      // PLAYER ROLL 1 - BUTTON //
+//********************************************************************************************//
+  $("form#player1Roll").submit(function(event) {
+    event.preventDefault();
+
+    let result = player1.rollDice();
+      if (result === false) { // If dice === 1
+        $("#player1Roll").hide();
+        $("#player1Hold").hide();
+        $("#player2Roll").show();
+        $("#player2Hold").show();
+      } else {
+        $("#turnScorePlayer1").text(player1.turnScore); // Holding tempScore while player continues to roll
+      }
+//********************************************************************************************//
+      // PLAYER ROLL 2 - BUTTON //
+//********************************************************************************************//
+  $("form#player2Roll").submit(function(event)  {
+    event.preventDefault();
+
+    let result = player2.rollDice()
+      if (result === false) {
+        $("player2Roll").hide();
+        $("player2Hold").hide();
+        $("player1Roll").show();
+        $("player1Hold").show();
+      }
+
+
+      
+
+      
+
+    $("#player1").text(player1.globalScore += player1.turnScore); // add turn score to total score
+    $("#player2").text(player2.globalScore += player2.turnScore);
+
+  })
+
+})
+
+
+
+
+// old code ------------
 
 function Player (name, turnTotal, overAll, active) {
-  this.name = name
+  this.name = name;
   this.turnTotal = 0;
   this.overAll = 0;
   this.active = active;
@@ -48,10 +120,3 @@ function rollEvaluator (roll) {
   }  
 }
 
-
-
-// User Interface //
-
-
-
-randomNumEvaluator()
